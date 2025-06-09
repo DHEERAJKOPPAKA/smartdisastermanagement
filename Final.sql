cab9b4646bd68bf1e5539660bd024b228a8d9694
Koppaka Dheeraj <112800474+DHEERAJKOPPAKA@users.noreply.github.com>
2025-06-09 12:25:36 +0530

    Add files via upload

---
+++ Final.sql
@@ -0,0 +1,12 @@
+drop table [dbo].[RiskAssessments]
+drop table [dbo].[EvacuationPlans]
+drop table [dbo].[DisasterIntimations]
+drop table [dbo].[Citizens]
+drop table [dbo].[Alerts]
+drop table [dbo].[Admins]
+drop table [dbo].[ActivityLogs]
+drop table [dbo].[__EFMigrationsHistory]
+
+select * from sys.tables
+
+use finaldb
